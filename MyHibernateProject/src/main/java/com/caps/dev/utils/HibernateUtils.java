82306ea8c3bcfecf9443188fc9e8f276e33ddb01
user <user@MUSTAQ-PC>
2018-10-28 18:07:51 +0530

    code added

---
+++ MyHibernateProject/src/main/java/com/caps/dev/utils/HibernateUtils.java
@@ -0,0 +1,20 @@
+package com.caps.dev.utils;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class HibernateUtils {
+
+	private HibernateUtils() {}
+	
+	private static final EntityManagerFactory emf = retrieveEMF();
+			
+	
+	private static EntityManagerFactory retrieveEMF() {
+		return Persistence.createEntityManagerFactory("myPersistenceUnit");
+	}
+	
+	public static EntityManagerFactory getEMF() {
+		return emf;
+	}
+}
